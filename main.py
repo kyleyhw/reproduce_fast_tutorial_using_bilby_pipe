841dd4caa219c782fa7bb0bc5bd9c65af1ee3563
Kyle <kyleyhw@gmail.com>
2023-06-06 12:05:48 -0400

    changed chirp mass to chirp mass source, added 90% credibility regions

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ posteriors = ['q_post', 'm_post', 'dL_post', 'thetajn_post']
 
 post_dict = {}
 injected_dict = {}
+lower_quantiles_dict = {}
+upper_quantiles_dict = {}
 
 for i in range(dims):
     print('injected %s =' %parameters[i], f['injection_parameters'][parameters[i]][()])
     post_dict[posteriors[i]] = f['posterior'][parameters[i]][:]
     injected_dict[parameters[i]] = f['injection_parameters'][parameters[i]][()]
+    lower_quantiles_dict[parameters[i]] = np.quantile(post_dict[posteriors[i]], 0.05)
+    upper_quantiles_dict[parameters[i]] = np.quantile(post_dict[posteriors[i]], 0.95)
 
 data = np.array(list(post_dict.values())).T
 injected_values = np.array(list(injected_dict.values()))
+lower_quantiles = np.array(list(lower_quantiles_dict.values()))
+upper_quantiles = np.array(list(upper_quantiles_dict.values()))
 
 fig = corner.corner(data, labels=[
         r"$q$",
@@ -36,7 +42,11 @@ post_best_estimates = np.median(data, axis=0)
 
 corner.overplot_lines(fig, injected_values, color='blue')
 corner.overplot_points(fig, injected_values[None], marker='s', color='blue', label='injected value')
+
 corner.overplot_lines(fig, post_best_estimates, color='orange')
 corner.overplot_points(fig, post_best_estimates[None], marker='s', color='orange', label='best estimate')
+
+corner.overplot_lines(fig, lower_quantiles, color='red')
+corner.overplot_lines(fig, upper_quantiles, color='red')
 
 fig.savefig('corner_plot.png')
